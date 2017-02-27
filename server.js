2d96884c040c7698d17e07f72c002654bd2a55b3
Renjie Zhou <zhour14@wfu.edu>
2017-02-27 02:41:15 -0500

    update on server.js

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-var express = require('express')
-var app = express()
+// loading necessary modules
+var express = require('express');
+var httpModule = require('http');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+// Create an express app
+var app = express();
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+var http = httpModule.Server(app);
+
+// Tells app that pictures, etc are located in folder
+// assets
+app.use(express.static('assets'));
+
+function responder(req, res) {
+  // Sending a file to the user's browser
+  res.sendFile(__dirname + '/test.html');
+  // print message in the server side console
+  console.log('got a request');
+};
+
+// Get request to / is given to responder function
+app.get('/', responder);
+
+function portListener() {
+    console.log('Listening on localhost ' + port);
+};
+
+var port = process.env.PORT ||  3000;
+http.listen(port, portListener);
